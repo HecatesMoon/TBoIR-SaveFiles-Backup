600788124821b70a5fb2f77a140136800afab9d4
JoacoOrbit <88943020+JoacoOrbit@users.noreply.github.com>
2026-01-25 16:14:57 -0300

    Make SaveManager Class with a simple test implementation

--- src/main/java/com/isaac/App.java
+++ src/main/java/com/isaac/App.java
@@ -1,9 +1,12 @@
 package com.isaac;
 
+import com.isaac.service.SaveManager;
+
 public class App {
     public static void main(String[] args) {
 
         System.out.println("Ruta de origen: " + Config.ORIGIN_PATH);
         System.out.println("Ruta de origen: " + Config.BACKUP_PATH);
+        SaveManager.backup();
     }
 }
--- src/main/java/com/isaac/service/SaveManager.java
+++ src/main/java/com/isaac/service/SaveManager.java
@@ -1,5 +1,18 @@
 package com.isaac.service;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.stream.Stream;
+
+import com.isaac.Config;
+
 public class SaveManager {
-    
+    public static void backup(){
+        try (Stream<Path> saveFiles = Files.walk(Config.ORIGIN_PATH)) {
+            saveFiles.forEach(file -> System.err.println(file));
+        } catch (IOException e) {
+            System.out.println("Failed trying to access folder: " + e.getMessage());
+        }
+    }
 }
